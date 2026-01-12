6592b06cec34b82b535adac212128dcb1d19f820
Foreman-b <hamzat4life@gmail.com>
2026-01-12 17:15:04 +0100

    Add login, logout and register to my templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Let get all list of books
 def list_books(request):
@@ -21,3 +26,38 @@ class LibraryDetailView(DetailView):
         
         context['books'] = self.object.books.all()
         return context
+
+# Let allow user to register
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+        # If form is NOT valid, it skips the if-block above
+    else:
+        form = UserCreationForm()
+    
+    # This return MUST be outside the if/else blocks to catch all cases
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# Let allow user to login
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# Let logout the user
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
