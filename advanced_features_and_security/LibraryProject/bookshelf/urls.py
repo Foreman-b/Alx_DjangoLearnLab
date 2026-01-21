6c7888cfd0b28674df7cd0450bb910d360fbac1d
Foreman-b <hamzat4life@gmail.com>
2026-01-21 10:33:22 +0100

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('book_list/', views.book_list, name='book_list'),
     path('can_view_book/', views.can_view_book, name='can_view_book'),
     path('can_create_book/', views.can_create_book, name='can_create_book'),
     path('can_edit_book/<int:pk>/', views.can_edit_book, name='can_edit_book'),
