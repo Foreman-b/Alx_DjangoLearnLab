a68cc0f73865a9135d4ea55f1d1995fbe2ae8cc8
Foreman-b <hamzat4life@gmail.com>
2026-02-09 12:39:07 +0100

    Creating UserCreationForm to handle user Registration, create the login in views.py create necessary templates like profile, register, login, profile and include the endpoint in urls.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from .models import Post
 
-# Create your views here.
+
+# Let handle user registration
+def home(request):
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, "blog/home.html", {'posts':posts})
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = CustomUserCreationForm()
+        return render(request, "blog/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("profile")
+    else:
+        form = AuthenticationForm()
+
+        return render(request, "blog/login.html", {"form":form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
-
 # TEMPLATES = [
 #     {
 #         'DIRS': [BASE_DIR / 'templates']
