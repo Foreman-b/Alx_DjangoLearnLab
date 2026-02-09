4e69c78fe70a5f814d8c5f051906ab4fcc12d5ca
Foreman-b <hamzat4life@gmail.com>
2026-02-09 20:44:13 +0100

    Fix a bug in my urls.py to find path post not posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,15 +6,15 @@ from .views import (
 
 
 urlpatterns = [
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-edit"),
+    path("post/new", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/", PostListView.as_view(), name="posts"),
     path("", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("profile/", views.profile, name="profile"),
-    path("posts/", PostListView.as_view(), name="posts"),
-    path("post/new", PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-edit"),
-    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-
+    path("profile/", views.profile, name="profile"),    
+    
 ]
