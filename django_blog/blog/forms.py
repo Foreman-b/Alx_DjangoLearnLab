8c82a5bfda31b52a69f28c996f497270f2fb817f
Foreman-b <hamzat4life@gmail.com>
2026-02-10 17:47:34 +0100

    Create CommentForm in forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -16,3 +17,9 @@ class CustomUserCreationForm(UserCreationForm):
             if commit:
                 user.save()
             return user
+        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
