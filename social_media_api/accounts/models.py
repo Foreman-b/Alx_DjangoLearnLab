6e03872d7a9e079efb28d2390ccd6d48b275ddb6
Foreman-b <hamzat4life@gmail.com>
2026-02-16 16:48:35 +0100

    Create user models, add Auth model user to settings, register the user in admin.py include the accounts.urls to social_media_api urls, create urls and serializers file in accounts app

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.settings import User
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField()
+    prifile_picture = models.ImageField(upload_to='uploads/profile_picture/')
+    followers = models.ManyToManyField(
+        "self", 
+        symmetrical=False, 
+        related_name="following", 
+        blank=True)
+
+
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.User'
