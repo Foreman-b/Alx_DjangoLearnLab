63f91b7235443518df7439f2ad4a5f8757da8c76
Foreman-b <hamzat4life@gmail.com>
2026-02-17 21:00:40 +0100

    Add serializers, permissions and urls to posts app, add pagnation to settings.py through rest_framework, then posts.urls now added to social_media_api.urls

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
