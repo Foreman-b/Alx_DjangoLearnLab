3a12202ccbabe13a219c5f184c7a5409ec164a49
Foreman-b <hamzat4life@gmail.com>
2026-02-18 14:14:40 +0100

    Add Like to models, register it on admin.py and add db.sqlite3-journal to gitignore

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -33,3 +33,23 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='user_likes'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+    
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
