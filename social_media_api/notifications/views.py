deffeced962ab96e87f26c2ae8c64ac05deeb250
Foreman-b <hamzat4life@gmail.com>
2026-02-18 17:08:16 +0100

    Add serializer to notifications app, create view for notifications, give route to it in notifications.urls and add urls from notifications to social_media_api.urls

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .serializers import NotificationSerializer
+from .models import Notification
+from rest_framework import permissions, generics, status
 
-# Create your views here.
+
+class NotificationListView(generics.ListAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_queryset(self):
+        """Let filter the notifications so that each user only see their own
+        orderby "-timestap (which shows it in descending order last notification to come first)
+        """
+        return Notification.objects.filter(recipient=self.request.user).order_by('-timestamp')
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')), 
-    path('api/', include('posts.urls')), 
+    path('api/', include('posts.urls')),
+    path('notifications/', include('notifications.urls')),
 ]
 
 if settings.DEBUG:
